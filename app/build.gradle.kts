b99531456fbdc7d9adda2227acb4c0a4e66bc6b1
Sayedts7 <sayedts00777@gmail.com>
2025-09-20 16:05:42 +0500

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.github_pr_project"
+        applicationId = "com.stinnovations.github_pr_project"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,7 +34,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -44,5 +44,3 @@ flutter {
 }
 
 
-
-Added login screen
